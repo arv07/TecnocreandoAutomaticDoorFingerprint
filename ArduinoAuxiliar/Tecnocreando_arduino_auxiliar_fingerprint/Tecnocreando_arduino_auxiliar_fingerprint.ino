c853808f1e489de36cabd0d6bb99c9b6f744cda9
Andres Rico <andresrico07@gmail.com>
2022-06-20 15:19:29 -0800

    Searching and return position of fingerprint

--- ArduinoAuxiliar/Tecnocreando_arduino_auxiliar_fingerprint/Tecnocreando_arduino_auxiliar_fingerprint.ino
+++ ArduinoAuxiliar/Tecnocreando_arduino_auxiliar_fingerprint/Tecnocreando_arduino_auxiliar_fingerprint.ino
@@ -317,8 +317,84 @@ uint8_t getFingerprintEnroll() {
   limitFingerprint = 10;
 
   return true;
+ 
+}
+
+
+uint8_t getFingerprintID() {
+  
+  uint8_t p = finger.getImage();
+  switch (p) {
+    case FINGERPRINT_OK:
+      Serial.println("Image taken");
+      break;
+    case FINGERPRINT_NOFINGER:
+      //Serial.println("No finger detected");
+      return p;
+    case FINGERPRINT_PACKETRECIEVEERR:
+      Serial.println("Communication error");
+      return p;
+    case FINGERPRINT_IMAGEFAIL: 
+      Serial.println("Imaging error");
+      return p;
+    default:
+      Serial.println("Unknown error");
+      return p;
+  }
+
+  // OK success!
+  /*if(p == FINGERPRINT_NOFINGER)
+  {
+    Serial.println("No finger print");  
+  }*/
 
   
+  delay(2000);
+  
+  p = finger.image2Tz();
+  
+  switch (p) {
+    case FINGERPRINT_OK:
+      Serial.println("Image converted");
+      break;
+    case FINGERPRINT_IMAGEMESS:
+      Serial.println("Image too messy");
+      return p;
+    case FINGERPRINT_PACKETRECIEVEERR:
+      Serial.println("Communication error");
+      return p;
+    case FINGERPRINT_FEATUREFAIL:
+      Serial.println("Could not find fingerprint features");
+      return p;
+    case FINGERPRINT_INVALIDIMAGE:
+      Serial.println("Could not find fingerprint features");
+      return p;
+    default:
+      Serial.println("Unknown error");
+      return p;
+  }
+
+  // OK converted!
+  p = finger.fingerSearch();
+  if (p == FINGERPRINT_OK) {
+    Serial.println("Found a print match!");
+  } else if (p == FINGERPRINT_PACKETRECIEVEERR) {
+    Serial.println("Communication error");
+    return p;
+  } else if (p == FINGERPRINT_NOTFOUND) {
+    Serial.println("Did not find a match");
+    return p;
+  } else {
+    Serial.println("Unknown error");
+    return p;
+  }
+
+  // found a match!
+  Serial.print("Found ID #"); Serial.print(finger.fingerID);
+  Serial.print(" with confidence of "); Serial.println(finger.confidence);
+  
+  //return finger.fingerID;
+  return true;
 }
 
 void loop()
@@ -365,6 +441,10 @@ void loop()
     }
     else if(opt == 3)
     {
+      while(! getFingerprintID() );
+    }
+    else if(opt == 4)
+    {
       Serial.print("Void Position: "); Serial.println(positionToStore);
     }
   else
@@ -374,6 +454,8 @@ void loop()
   }
  
 }
- 
+
+getFingerprintID();
+delay(50);
   
 }
